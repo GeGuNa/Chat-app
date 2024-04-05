332e083d9630b8b40cc6cb125f36aa0c58fad940
Phantom <abracadabra1253@gmail.com>
2024-04-06 03:39:31 +0400

    Login redirection url

--- myproject/settings.py
+++ myproject/settings.py
@@ -138,7 +138,8 @@ STATIC_ROOT = BASE_DIR / "myapp/static/"
 MEDIA_ROOT = BASE_DIR / "myapp/media/"
 
 
-
+LOGIN_REDIRECT_URL = '/' 
+LOGIN_URL = '/' ##login url 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
